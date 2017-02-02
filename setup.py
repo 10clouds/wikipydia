195296b771fc075257197b91c7ae69a7457fe367
Maciej Gol <1kroolik1@gmail.com>
2017-02-02 17:53:06 +0000

    Bumped version

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
- 
+
 from distutils.core import setup
- 
-setup(name='wikipydia',
-      version='0.1.6',
-      description='Python Client Library for the Wikipedia API',
-      author='James Dennis',
-      author_email='jd@j2labs.net',
-      url='http://github.com/j2labs/wikipydia',
-      packages=['wikipydia'])
+
+setup(
+    author='James Dennis',
+    author_email='jd@j2labs.net',
+    description='Python Client Library for the Wikipedia API',
+    install_requires=['requests'],
+    name='wikipydia',
+    packages=['wikipydia'],
+    url='http://github.com/j2labs/wikipydia',
+    version='0.1.7',
+)
